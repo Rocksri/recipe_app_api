77e13b22dd80ce2cace7dbe45bdb1bbf33408a01
Rocksri <srinathanand1979@gmail.com>
2024-09-29 20:35:33 +0530

    added superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,9 +7,10 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
     """Test models."""
+
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
-        email = 'test@test.com'
+        email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
@@ -22,9 +23,9 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
-            ['test1@EXAMPLE.COM', 'test1@example.com'],
+            ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.com', 'TESET3@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
@@ -37,10 +38,12 @@ class ModelTests(TestCase):
             get_user_model().objects.create_user('', 'test123')
 
     def test_create_superuser(self):
-        """Test create a superuser"""
+        """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
             'test@example.com',
             'test123',
         )
+
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
