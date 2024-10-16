09c652afe485f897b77c64a2abf5f01a32d089fa
Rocksri <srinathanand1979@gmail.com>
2024-10-16 16:05:45 +0530

    create recipe

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -134,8 +134,8 @@ class PrivateUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, {
-            'name', self.user.name,
-            'email', self.user.email,
+            'name': self.user.name,
+            'email': self.user.email,
         })
 
     def test_post_me_mot_allowed(self):
